351979dacbe66e18aa477c2cb1cb1d6b256ae60c
contessa <tessa.fallon@gmail.com>
2013-11-13 12:31:21 -0500

    Add teenager-test files

---
+++ teenager_solution.rb
@@ -0,0 +1,7 @@
+#Write a method called is_a_teenager that takes age as a parameter and returns
+#true/false depending on whether the age indicates that the person is a 
+#teenager
+
+def is_a_teenager? age
+  age > 12 && age < 20
+end
